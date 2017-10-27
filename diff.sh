2baf14f51401f75dd597dfd8bbc30308bd4b2ed6
Michael Studer <mike_studer@apple.com>
2017-10-27 12:54:55 -0700

    Remove Solaris OS, updated commands

--- diff.sh
+++ diff.sh
@@ -7,17 +7,13 @@ dotdiff() {
    find . -type f |sed 's/^.//g'|xargs -I{} sh -c 'echo {};diff .{} ~/{};echo'
 }
 
-read -p "Select OS, OSX (1), Linux (2), Solaris (3): " myos
+read -p "Select OS, OSX (1), Linux (2): " myos
 if [ $myos -eq 1 ]; then
 	ospath="./OSX"
 	cd $ospath
         dotdiff
 elif [ $myos -eq 2 ]; then
 	ospath="./Linux"
-	cd $ospath
-        dotdiff
-elif [ $myos -eq 3 ]; then
-	ospath="./Solaris"
 	cd $ospath
         dotdiff
 else
---
+++ dotupdater.py
@@ -0,0 +1,9 @@
+#!/usr/bin env python
+#
+# Program to check for dot file differences and update
+# as desired.
+#
+# Written by Mike Studer
+# Oct 2017
+#
+

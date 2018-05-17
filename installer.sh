10e61cafbd91621e59c594823e2ad88575b4b29d
Michael Studer <mike_studer@apple.com>
2018-05-17 13:57:40 -0700

    Automated installer.sh OS detection

--- installer.sh
+++ installer.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 # Install dot files to local system
 #
-echo "Dumb Installer.  Copies mydotfiles to home directory"
-read -p "Select OS, OSX (1), Linux (2), Exit(3): " myos
-if [ $myos -eq 1 ]; then
+# read -p "Select OS, OSX (1), Linux (2): " myos
+myos="$(uname -s)"
+echo "OS detected = $myos"
+if [ $myos == 'Darwin' ]; then
 	ospath="./OSX/"
-elif [ $myos -eq 2 ]; then
+elif [ $myos == 'Linux' ]; then
 	ospath="./Linux/"
 else
 	echo "No changes made. Exiting"
 	exit 1
 fi
-
 cd $ospath
 cp ./.* ~

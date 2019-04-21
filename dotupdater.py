2d513960d6437e6b2b782a5742e0372ae24efdea
mstuder95376 <mike@hamsoftware.org>
2019-04-20 20:46:50 -0700

    Fixed bugs

--- dotupdater.py
+++ dotupdater.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 #
 # Program to check for dot file differences and update
 # as desired.
@@ -13,13 +13,14 @@ import sys
 import os
 from os.path import expanduser
 
-print "This utility checks differences between mydotfiles and users home directory"
-print "It gives you the option to replace the files.  It will ask what to do for each file."
-print ""
-useros = raw_input("Please select OS, o = OSX, l = Linux: ")
-if useros == 'l':
+print("This utility checks differences between mydotfiles and users home directory")
+print("It gives you the option to replace the files.  It will ask what to do for each file.")
+print("")
+# useros = input("Please select OS, o = OSX, l = Linux: ")
+useros = os.uname()
+if useros[0] == 'Linux':
     ospath = "./Linux"
-elif useros == 'o':
+elif useros[0] == 'Darwin':
     ospath = "./OSX"
 else:
     exit(1)
@@ -28,45 +29,52 @@ home = expanduser("~")
 # Everything is based on what is in the mydotfiles directory
 files = [f for f in os.listdir('.') if os.path.isfile(f)]
 for f in files:
-    f2 = home + '/' + f
+    f2 = home + '/' + f  # f2 is home dir files
 #    print "DEBUG" , f , f2
-    if (filecmp.cmp( f, f2 )):
-        print "Files match,", f
+    if (filecmp.cmp(f, f2) and os.path.isfile(f2)):
+        print("Files match," + f)
     else:
-        print
-        print "*** Mismatch,", f, " ***"
-        print "host0 is", f
-        print "host1 is", f2
-        # Do a diff at this point.  
-        with open(f, 'r') as hosts0:
-            with open(f2, 'r') as hosts1:
-                diff = difflib.unified_diff(
-                 hosts0.readlines(),
-               hosts1.readlines(),
-               fromfile='hosts0',
-               tofile='hosts1',
-             )
-                for line in diff:
-                    sys.stdout.write(line) 
-        print
+        print()
+        print("*** Mismatch, ", f, " ***")
+        print("git_file is " + f)
+        if os.path.isfile(f2):
+            print("local_file is " + f2)
+            # Do a diff at this point.
+            with open(f, 'r') as git_file:
+                with open(f2, 'r') as local_file:
+                    diff = difflib.unified_diff(
+                     git_file.readlines(),
+                     local_file.readlines(),
+                     fromfile='git_file',
+                     tofile='local_file',
+                    )
+                    for line in diff:
+                        sys.stdout.write(line)
+            print
+        else:
+            print(f + 'Not in home directory')
         # Now check what to do.
         # f = mydotfiles
         # f2 = ~
-        print "Select what to do with", f
-        print "1. Copy from mydotfiles to ~ "
-        print "2. Copy from ~ to mydotfiles"
-        print "3. Do nothing and exit"
-        kk = raw_input("Choice: ")
+        print("Select what to do with", f)
+        print("1. Copy from mydotfiles to ~ ")
+        print("2. Copy from ~ to mydotfiles")
+        print("3. Skip to next file")
+        print("4. Do nothing and exit")
+        kk = input("Choice: ")
         if kk == '1':
-            print "Copying mydotfiles to ~ "
+            print("Copying mydotfiles to ~ ")
             # src, dst
-            shutil.copy2(f,f2)
+            shutil.copy2(f, f2)
         elif kk == '2':
-            print "Copying ~ to mydotfiles "
-            # src, dst
-            shutil.copy2(f2,f)
+            if os.path.isfile(f2):
+                print("Copying ~ to mydotfiles ")
+                # src, dst
+                shutil.copy2(f2, f)
+            else:
+                print("You can't copy what does not exist!")
+        elif kk == '3':
+            print("Skipping file " + f)
         else:
-            print "No changes made. Exiting."
+            print("No changes made. Exiting.")
             exit(1)
-
-

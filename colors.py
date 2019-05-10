480617b38a773aa58a69ee190578a947edd0dd74
Michael Studer <mike_studer@apple.com>
2019-05-10 15:05:26 -0700

    Adding color

---
+++ colors.py
@@ -0,0 +1,77 @@
+class Colors(object):
+    '''
+    Colors class:
+    Reset all colors with colors.reset
+    To enable/disable color invoke with boolean, True/False
+    ie., cstate is a boolean to control whether color text is used or not.
+    '''
+
+    def __init__(self, cstate):
+        self.cstate = cstate
+
+        if cstate:
+            self.reset = '\033[0m'
+            self.bold = '\033[01m'
+            self.disable = '\033[02m'
+            self.underline = '\033[04m'
+            self.reverse = '\033[07m'
+            self.strikethrough = '\033[09m'
+            self.invisible = '\033[08m'
+            # Foreground colors
+            self.fg_black = '\033[30m'
+            self.fg_red = '\033[31m'
+            self.fg_green = '\033[32m'
+            self.fg_orange = '\033[33m'
+            self.fg_blue = '\033[34m'
+            self.fg_purple = '\033[35m'
+            self.fg_cyan = '\033[36m'
+            self.fg_lightgrey = '\033[37m'
+            self.fg_darkgrey = '\033[90m'
+            self.fg_lightred = '\033[91m'
+            self.fg_lightgreen = '\033[92m'
+            self.fg_yellow = '\033[93m'
+            self.fg_lightblue = '\033[94m'
+            self.fg_pink = '\033[95m'
+            self.fg_lightcyan = '\033[96m'
+            # Background colors
+            self.bg_black = '\033[40m'
+            self.bg_red = '\033[41m'
+            self.bg_green = '\033[42m'
+            self.bg_yellow = '\033[43m'
+            self.bg_blue = '\033[44m'
+            self.bg_purple = '\033[45m'
+            self.bg_cyan = '\033[46m'
+            self.bg_lightgrey = '\033[47m'
+        else:
+            self.reset = ''
+            self.bold = ''
+            self.disable = ''
+            self.underline = ''
+            self.reverse = ''
+            self.strikethrough = ''
+            self.invisible = ''
+            # Foreground colors
+            self.fg_black = ''
+            self.fg_red = ''
+            self.fg_green = ''
+            self.fg_orange = ''
+            self.fg_blue = ''
+            self.fg_purple = ''
+            self.fg_cyan = ''
+            self.fg_lightgrey = ''
+            self.fg_darkgrey = ''
+            self.fg_lightred = ''
+            self.fg_lightgreen = ''
+            self.fg_yellow = ''
+            self.fg_lightblue = ''
+            self.fg_pink = ''
+            self.fg_lightcyan = ''
+            # Background colors
+            self.bg_black = ''
+            self.bg_red = ''
+            self.bg_green = ''
+            self.bg_yellow = ''
+            self.bg_blue = ''
+            self.bg_purple = ''
+            self.bg_cyan = ''
+            self.bg_lightgrey = ''
--- dotupdater.py
+++ dotupdater.py
@@ -16,18 +16,36 @@ import os
 import tarfile
 from os.path import expanduser
 from pathlib import Path
+from colors import Colors
 
 # f_git = mydotfiles
 # f_home = ~
+klr = Colors(True)
 
-print("This utility checks differences between mydotfiles and users home directory")
-print("It gives you the option to replace the files.  It will ask what to do for each file.")
+
+def sepline():
+    for i in list(range(40)):
+        print(klr.fg_lightgrey + "=", end="")
+        print(klr.fg_darkgrey + "=", end="")
+    print(klr.reset)
+
+
+print(
+    klr.fg_blue
+    + "This utility checks differences between mydotfiles and users home directory"
+    + klr.reset
+)
+print(
+    klr.fg_blue
+    + "It gives you the option to replace the files.  It will ask what to do for each file."
+    + klr.reset
+)
 print("")
 # useros = input("Please select OS, o = OSX, l = Linux: ")
 useros = os.uname()
-if useros[0] == 'Linux':
+if useros[0] == "Linux":
     ospath = "./Linux"
-elif useros[0] == 'Darwin':
+elif useros[0] == "Darwin":
     ospath = "./OSX"
 else:
     print("Unable to determine OS.  Exiting.")
@@ -35,58 +53,75 @@ else:
 os.chdir(ospath)
 home = expanduser("~")
 # Everything is based on what is in the mydotfiles directory
-files = [f for f in os.listdir('.') if os.path.isfile(f)]
+files = [f for f in os.listdir(".") if os.path.isfile(f)]
 for f_git in files:
-    f_home = home + '/' + f_git  # f_home is home dir files
+    f_home = home + "/" + f_git  # f_home is home dir files
     #    print "DEBUG" , f_git , f_home
     # Need to test if a file by that name exists in home dir.
     home_file = Path(f_home)
     # Check if the file exists in the home directory
     if home_file.is_file():
         if filecmp.cmp(f_git, f_home):
-            print("Files match, " + f_git)
+            print(klr.fg_green + "Files match, " + f_git + klr.reset)
         else:
             print()
-            print("*** Mismatch between, {} and {} ".format(f_git, f_home))
+            print(
+                klr.fg_red
+                + klr.bold
+                + "*** Mismatch *** between, "
+                + klr.fg_cyan
+                + f_git
+                + klr.reset
+                + " and "
+                + klr.fg_purple
+                + f_home
+                + klr.reset
+            )
             print("Displaying file diffs...")
-            with open(f_git, 'r') as git_file:
-                with open(f_home, 'r') as local_file:
+            with open(f_git, "r") as git_file:
+                with open(f_home, "r") as local_file:
                     diff = difflib.unified_diff(
                         git_file.readlines(),
                         local_file.readlines(),
-                        fromfile='git_file',
-                        tofile='local_file',
+                        fromfile="git_file",
+                        tofile="local_file",
                     )
                     for line in diff:
                         sys.stdout.write(line)
             print
     else:
         print("{} is not in home directory".format(f_git))
-            # Now let user select what to do.
-    print("==============================")
-    print("Please select what to do with {}".format(f_git))
+        # Now let user select what to do.
+    sepline()
+    print(
+        "Please select what to do with "
+        + klr.fg_yellow
+        + "{}".format(f_git)
+        + klr.reset
+    )
+    sepline()
     print("1. Copy git file to ~ ")
     if home_file.is_file():
-        print("2. Copy from ~ to git dir")
+        print("2. Copy from home to git directory")
     print("3. Skip to next file")
     print("4. Do nothing and exit")
-    if 'tgz' in f_git:
+    if "tgz" in f_git:
         print("5. Copy and extract {}".format(f_git))
     kk = input("Choice: ")
-    if kk == '1':
-        print("Copying git file to ~ ")
+    if kk == "1":
+        print("Copying git file to home ")
         # src, dst
         shutil.copy2(f_git, f_home)
-    elif kk == '2':
+    elif kk == "2":
         if home_file.is_file():
-            print("Copying ~ to git dir ")
+            print("Copying home to git directory ")
             # src, dst
             shutil.copy2(f_home, f_git)
         else:
             print("You can't copy what does not exist!")
-    elif kk == '3':
+    elif kk == "3":
         print("Skipping file " + f_git)
-    elif kk == '5':
+    elif kk == "5":
         print("Copying {} to home directory".format(f_git))
         shutil.copy2(f_git, f_home)
         # Now extract it

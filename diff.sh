c5ce29a804fc8d8b2d9c62f14b2d2c52918f4b86
Mike Studer <mike@hamsoftware.org>
2024-02-07 17:33:09 -0800

    Short diff output

--- diff.sh
+++ diff.sh
@@ -4,7 +4,7 @@
 dotdiff() {
    echo "Checking for differences in dot files"
    echo "-------------------------------------"
-   find . -type f |sed 's/^.//g'|xargs -I{} sh -c 'echo {};diff .{} ~/{};echo'
+   find . -type f |sed 's/^.//g'|xargs -I{} sh -c 'echo {};diff -q .{} ~/{};echo'
 }
 
 read -p "Select OS, OSX (1), Linux (2) or (3) to quit: " myos

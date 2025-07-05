e06579e2179c699f3fdc98efa1fe1e77cec34ffd
Mike Studer <mike@hamsoftware.org>
2025-07-05 09:37:40 -0700

    backing up things before I mess it us

--- shotme.sh
+++ shotme.sh
@@ -1,14 +1,14 @@
-#!/usr/bin/zsh
+#!/usr/bin/bash
 printf "\n"
 
 function logo ()
 {
-   lolcat ./logo.txt
+   lolcat --freq=0.4 ./logo.txt
 }
 
 function root_check ()
 {
-   if [ $(id -u) -ne 0 ]
+   if [ "$(id -u)" -ne 0 ]
      then echo Please run this script as root or using sudo!
      exit
    fi
@@ -18,11 +18,11 @@ function detect_os ()
 {
    detected_os="$(uname -o)"
    if [[ "$detected_os" =~ "Linux" ]]; then
-     echo "FOUND Linux"
+     #printf "FOUND Linux"
      detected_os="Linux"
    fi
    if [[ "$detected_os" =~ "Darwin" ]]; then
-     echo "Found OSX"
+     #printf "Found OSX"
      detected_os="OSX"
    fi
 }
@@ -31,47 +31,89 @@ function menu ()
 {
 gum style\
    --border normal\
-   --padding "1"\
-   --margin "2"\
+   --padding "0"\
+   --margin "1"\
    --border-foreground "#00ffff"\
    --border-background "#0000ff"\
    "$(gum style\
       --foreground "#00ff00"\
       --background "#000000"\
+      --align center \
+      --width 80\
       "$1")"
+}
+
+ function gmsg ()
+ {
+   gum style\
+      --foreground "#F0B27A"\
+      --background "#000000"\
+      --align center \
+      --width 80\
+      "$1"
 }
 
 function LinuxLoop ()
 {
-   while read p; do
-     echo "Checking for $p"
-     if [[ "$(pacman -Qk $p)" =~ "total files" ]]; then
-        echo "$p is installed"
-     else
-        echo "Installing $p"
-	pacman --noconfirm -S "$p"
-     fi
+   while read -r p; do
+    echo "Installing $p"
+	sudo pacman --noconfirm -S "$p" --needed
 #   done < ./Linux/PKGS.txt
    done < "$1"
 }
 
-# cd ./${detected_os}
-# bat $(find . -type f -name ".*" | gum choose --limit 2)
 
+# ================================
 # ============= Main =============
-# TEMP BYPASS # root_check
+# ================================
+
+# Check what OS this is running on, Linux or Mac
+detect_os
 echo "Installing basic packages so this script will work properly."
-# TEMP LinuxLoop "./Linux/BASIC_PKGS.txt"
+# LinuxLoop "./Linux/BASIC_PKGS.txt" # This works it just gets annoying after everything is in sync
 printf "Continuing ...\n\n\n"
 logo
-printf "\n\n"
+menu "Dotme Main Menu for $detected_os"
 
-menu "Dotme Main Menu"
-
+# Primary menu of operations
 ch=$(gum choose\
-   "1. Install these dot files"\
-   "2. Install vim directory"\
-   "3. Install preferred packages"\
-   "Q. Quit")
+   "Install these git dot files"\
+   "Install vim directory"\
+   "Install preferred packages"\
+   "Quit")
+
+# Now handle the users choice
+ if [[ "$ch" =~ "dot" ]]; then
+    gmsg "Installing dot files"
+# Need to move original users file to a new filename with a date extension
+    # Buggy... find "./$detected_os/.*" -maxdepth 1 -type f -exec cp -t ~/testdir {} "{}_$(date +%Y%m%d-%H:%M)" +
+    find "./$detected_os/.*" -maxdepth 1 -type f -exec cp -t ~/testdir  "{}_$(date +%Y%m%d-%H:%M)" +
+#    find "./$detected_os/.*" -maxdepth 1 -type f -exec cp -t ~/testdir {} +
+
+ elif [[ "$ch" =~ "vim" ]]; then
+    gmsg "Installed vim environment"
+    cp -r "./$detected_os/.vim/" ~/testdir
+
+ elif [[ "$ch" =~ "packages" ]]; then
+    printf "Installed packages\n"
+    if [[ "$detected_os" =~ "Linux" ]]; then
+       printf "DEBUG 1"
+       LinuxLoop "./Linux/PKGS.txt"
+       gmsg "Installed the packages"
+    elif [[ "$detected_os" =~ "OSX" ]]; then
+       gmsg "(Unsupported) Mac OS needs packages to be brew installed."
+    fi
+
+ elif [[ "$ch" =~ "Quit" ]]; then
+    gmsg "Exiting by request"
+
+ else
+    # Just in case something weird happens
+    menu "Unknown command, exiting!"
+    exit
+ fi
+
+# Parting friendly message
+menu "Thanks for coming by!"
 
 

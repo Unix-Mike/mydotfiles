c161cf89b2351227c9bfa0ef0a658c569cef11e4
Mike Studer <mike@hamsoftware.org>
2026-05-13 15:32:45 -0700

    wezterm = ghostty keybindings

--- Linux/.config/wezterm/wezterm.lua
+++ Linux/.config/wezterm/wezterm.lua
@@ -1,45 +1,70 @@
-local wezterm = require("wezterm")
+local wezterm = require('wezterm')
 local config = wezterm.config_builder()
 
--- font settings
+-- Font Settings
 config.font_size = 12
+config.line_height = 0.9
+config.initial_cols = 150
+config.initial_rows = 65
 -- config.font = wezterm.font 'FiraCode Nerd Font'
-config.font = wezterm.font("BlexMono Nerd Font")
+-- config.font = wezterm.font 'BlexMono Nerd Font'
+config.font = wezterm.font 'SauceCodePro Nerd Font'
 -- config.font = wezterm.font 'Monofur Nerd Font'
+
 
 -- Colors
 config.colors = {
-	cursor_bg = "white",
-	cursor_border = "white",
+cursor_bg = "white",
+cursor_border = "white",
+split = 'yellow',
 }
+
 
 -- Appearance
 config.window_padding = {
-	left = 0,
-	right = 0,
-	top = 0,
-	bottom = 0,
+   left = 0,
+   right = 0,
+   top = 0,
+   bottom = 0,
 }
 
--- config.window_background_opacity = 0.9
--- config.color_scheme = 'Tango (terminal.sexy)'
--- config.color_scheme = 'Apple Classic'
--- config.color_scheme = 'Apple System Colors'
--- config.color_scheme = 'Ibm3270 (Gogh)'
--- config.color_scheme = "Ibm 3270 (High Contrast) (Gogh)"
--- config.color_scheme = 'iTerm2 Default'
-config.color_scheme = 'Vibrant Ink (Gogh)'
+config.keys = {
+  -- This will create a new split and run your default program inside it
+  {
+    key = 'o',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+  },
+  {
+    key = 'e',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+  },
+}
 
-config.initial_cols = 250
-config.initial_rows = 60
+-- Color Schemes I like to use
+config.color_scheme = 'Snazzy'
 
+-- Window Transparency
+config.window_background_opacity = 0.90
+
+
+-- Window Frame
 config.window_frame = {
-	font = require("wezterm").font("Roboto"),
-	font_size = 12,
+  border_left_width = '0.25cell',
+  border_right_width = '0.25cell',
+  border_bottom_height = '0.15cell',
+  border_top_height = '0.15cell',
+  border_left_color = 'gray',
+  border_right_color = 'gray',
+  border_bottom_color = 'gray',
+  border_top_color = 'gray',
+  button_fg = 'white',
+  button_bg = 'blue',
+  font = require('wezterm').font 'Roboto',
+  font_size = 10,
 }
 
-config.colors = {
-	split = "yellow",
-}
+-- General Setting
 
 return config
